04d6509be3dd42be01403c777a86e1c1f8613b65
Erhel czc <erhelito@yahoo.fr>
2026-01-30 15:38:22 +0100

    Implement win condition check and display win message in Window class

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ class Window(arcade.Window):
     def setup(self):
         self.sprites = arcade.SpriteList()
         self.goos = []
+        self.won = False
 
         self.solids = []
 
@@ -38,11 +39,75 @@ class Window(arcade.Window):
         draw_links()
         self.sprites.draw()
 
+        if self.won:
+            arcade.draw_text("Gagné !", self.width // 2, self.height // 2,
+                             arcade.color.BLACK, 60, anchor_x="center", bold=True)
+        pass
+
+    def is_goo_near_platform(self, goo, platform):
+        """Check if a goo is near a platform (with some margin)"""
+        contact_margin = 50
+
+        dx = goo.center_x - platform.center_x
+        dy = goo.center_y - platform.center_y
+
+        if platform.shape == "ellipse":
+            # For an ellipse, use the ellipse equation
+            a = platform.width / 2 + contact_margin
+            b = platform.height / 2 + contact_margin
+            return (dx/a)**2 + (dy/b)**2 < 1
+
+        else:  # rectangle
+            # For a rectangle, check if within the bounding box
+            half_width = platform.width / 2 + contact_margin
+            half_height = platform.height / 2 + contact_margin
+            return abs(dx) < half_width and abs(dy) < half_height
+
+    def check_win_condition(self):
+        """Check if any goo has reached the end platform"""
+        if self.won or not self.goos:
+            return False
+
+        # Find goos touching the start platform
+        start_goos = []
+        for goo in self.goos:
+            if self.is_goo_near_platform(goo, self.start):
+                start_goos.append(goo)
+
+        if not start_goos:
+            return False
+
+        visited = set()
+        queue = start_goos.copy()
+
+        for goo in queue:
+            visited.add(goo)
+
+        while queue:
+            current_goo = queue.pop(0)
+
+            # Check if the goo touches the end platform
+            if self.is_goo_near_platform(current_goo, self.end):
+                return True
+
+            # add connected goos to the queue
+            for other_goo in self.goos:
+                if other_goo not in visited:
+                    # check if there is a spring connection
+                    if (current_goo, other_goo) in Goo.rest_lengths:
+                        visited.add(other_goo)
+                        queue.append(other_goo)
+
+        return False
+
     def on_update(self, delta_time):
         # Move the goos while considering collisions
         for goo in self.goos:
             goo.move(self.solids)
         self.sprites.update()
+
+        if not self.won and self.check_win_condition():
+            self.won = True
 
     def on_mouse_press(self, x, y, button, modifiers):
         if button == arcade.MOUSE_BUTTON_LEFT:
