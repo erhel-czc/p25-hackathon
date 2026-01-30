5ba0dde89750aec37e00e100d2e6de593ae6fe84
groussetlolaetudes-creator <lola.grousset.etudes@gmail.com>
2026-01-30 11:07:18 +0100

    simulation aleatoire de la position du depart et de l'arrivee et choix entre un triangle et un carre

--- modules/solid.py
+++ modules/solid.py
@@ -1,2 +1,29 @@
 import numpy as np
+import arcade
+from random import randint, choice
+from PIL import Image, ImageDraw
 
+
+class Solid(arcade.Sprite):
+    def __init__(self, x, y, solid_size=40):
+        super().__init__()
+
+        self.center_x = x
+        self.center_y = y
+        self.solid_size = solid_size
+
+        # Choix aléatoire de la forme
+        shape = choice(["circle", "rectangle"])
+
+        if shape == "circle":
+            self.texture = arcade.make_circle_texture(
+                solid_size,
+                arcade.color.DARK_GREEN
+            )
+
+        elif shape == "rectangle":
+            self.texture = arcade.make_soft_square_texture(
+                solid_size,
+                arcade.color.DARK_BLUE,
+                outer_alpha=255
+            )
